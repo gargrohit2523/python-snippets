09aeae830780cf19d9804d0a9ba708785baa1f9f
Rohit X1C <rohitgarg11@outlook.com>
2024-01-03 21:36:53 +0530

    2 plots high & low temps

--- datavisualization/visualizemaxtemp.py
+++ datavisualization/visualizemaxtemp.py
@@ -4,33 +4,41 @@ import matplotlib.pyplot as mplt
 from datetime import datetime
 
 path = Path("datasets\\delhi_temp_1951_2020.csv")
-contents = path.read_text().splitlines()
 
+#read latest 2 year data by selecting last items from content which reduces data size and improves performance before plotting
+contents = path.read_text().splitlines()[-730:]
 reader = csv.reader(contents)
 #read header row by calling next
 header_row = next(reader)
 
 highs = []
+lows = []
 dates = []
 
 for row in reader:
     max_temp = 0
+    min_temp = 0
 
     try:
         max_temp = round(float(row[3]))
+        min_temp = round(float(row[4]))
     except:
         pass
 
     date = datetime.strptime(row[1], '%Y-%m-%dT%H:%M:%S')
 
     highs.append(max_temp)
+    lows.append(min_temp)
     dates.append(date)
 
 #mplt.style.use('seaborn')
 figs,ax = mplt.subplots()
-ax.plot(dates[-30:], highs[-30:], color='red')
 
-ax.set_title("Daily High Temprature, 2020 Last 2 months, Delhi")
+#Take last 2 years data from dataset
+ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='red')
+
+ax.set_title("Daily High & Low Tempratures, 2019-2020, Delhi")
 
 ax.set_xlabel('')
 ax.set_ylabel("Temprature (C)")
